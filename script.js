c77a9d7317099b7163d89236507bd60ad7143d14
ThiagoQuetz <lizplanejadosjf@gmail.com>
2024-10-18 16:05:54 -0300

    first-commit-lizplanejados

---
+++ script.js
@@ -0,0 +1,22 @@
+    const form = document.querySelector("")
+
+
+
+function Enviar() {
+
+    const nome = document.getElementById("nomeid");
+
+
+    if (nome.value != "") {
+        alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
+    }else{
+        alert('Preenchimento dos campos é obrigatório!')
+    }
+
+}
+
+function navegar() {
+    const btn1 = document.querySelector("#bt-nossos-servicos")
+
+    
+}
